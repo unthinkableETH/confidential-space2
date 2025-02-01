2533422ee82600f4a6cf2315c8b37797fa7f9cdb
Shaun <158432022+unthinkableETH@users.noreply.github.com>
2025-01-31 22:02:02 -0500

    Update create_service_accounts.sh

--- codelabs/health_data_analysis_codelab/scripts/create_service_accounts.sh
+++ codelabs/health_data_analysis_codelab/scripts/create_service_accounts.sh
@@ -6,18 +6,10 @@ source config_env.sh
 source common.sh
 source create_workload_service_account.sh
 
-set_gcp_project "${USLEEP_PROJECT_ID}"
+set_gcp_project "${WEB3DATA_PROJECT_ID}"
 
-echo "Creating workload service-account ${USLEEP_WORKLOAD_SERVICE_ACCOUNT} under project ${USLEEP_PROJECT_ID}..."
-create_service_account "${USLEEP_WORKLOAD_SERVICE_ACCOUNT}"
-grant_service_account_user_role_to_workload_operator "${USLEEP_WORKLOAD_SERVICE_ACCOUNT}" "${USLEEP_PROJECT_ID}"
-grant_workload_user_role_to_service_account "${USLEEP_WORKLOAD_SERVICE_ACCOUNT}" "${USLEEP_PROJECT_ID}"
-grant_log_writer_role_to_service_account "${USLEEP_WORKLOAD_SERVICE_ACCOUNT}" "${USLEEP_PROJECT_ID}"
-
-set_gcp_project "${UWEAR_PROJECT_ID}"
-
-echo "Creating workload service-account ${UWEAR_WORKLOAD_SERVICE_ACCOUNT} under project ${UWEAR_PROJECT_ID}..."
-create_service_account "${UWEAR_WORKLOAD_SERVICE_ACCOUNT}"
-grant_service_account_user_role_to_workload_operator "${UWEAR_WORKLOAD_SERVICE_ACCOUNT}" "${UWEAR_PROJECT_ID}"
-grant_workload_user_role_to_service_account "${UWEAR_WORKLOAD_SERVICE_ACCOUNT}" "${UWEAR_PROJECT_ID}"
-grant_log_writer_role_to_service_account "${UWEAR_WORKLOAD_SERVICE_ACCOUNT}" "${UWEAR_PROJECT_ID}"
+echo "Creating workload service-account ${WEB3DATA_WORKLOAD_SERVICE_ACCOUNT} under project ${WEB3DATA_PROJECT_ID}..."
+create_service_account "${WEB3DATA_WORKLOAD_SERVICE_ACCOUNT}"
+grant_service_account_user_role_to_workload_operator "${WEB3DATA_WORKLOAD_SERVICE_ACCOUNT}" "${WEB3DATA_PROJECT_ID}"
+grant_workload_user_role_to_service_account "${WEB3DATA_WORKLOAD_SERVICE_ACCOUNT}" "${WEB3DATA_PROJECT_ID}"
+grant_log_writer_role_to_service_account "${WEB3DATA_WORKLOAD_SERVICE_ACCOUNT}" "${WEB3DATA_PROJECT_ID}"
